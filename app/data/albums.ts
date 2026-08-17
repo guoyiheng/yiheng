9ab3762e757c3b154b9054fef3a0f77e00610d13
yiheng <hi@yiheng.run>
2026-08-17 17:07:36 +0800

    feat: 更新喜欢的音乐列表并清理指定艺人专辑

--- app/data/albums.ts
+++ app/data/albums.ts
@@ -29,41 +29,6 @@ export const favouriteAlbums: FavouriteAlbum[] = [
     appleMusicUrl: 'https://music.apple.com/cn/album/%E5%90%8C%E5%90%8D%E5%B0%88%E8%BC%AF/535790918'
   },
   {
-    name: '忘记你我做不到',
-    artist: '张学友',
-    releasedAt: '1996',
-    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/2d/95/9e/2d959ebd-1d4b-c0f4-bd27-bbbd86a590ea/06UMGIM30503.rgb.jpg/600x600bb.jpg',
-    appleMusicUrl: 'https://music.apple.com/cn/album/%E5%BF%98%E8%A8%98%E4%BD%A0%E6%88%91%E5%81%9A%E4%B8%8D%E5%88%B0/1444065611'
-  },
-  {
-    name: '吻别',
-    artist: '张学友',
-    releasedAt: '1993',
-    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music118/v4/bf/c1/93/bfc19337-d44e-9bbc-2878-dec47d1c645d/00731451920820.rgb.jpg/600x600bb.jpg',
-    appleMusicUrl: 'https://music.apple.com/cn/album/%E5%90%BB%E5%88%A5/1443431909'
-  },
-  {
-    name: '遥远的她',
-    artist: '张学友',
-    releasedAt: '1986',
-    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/ea/6e/18/ea6e186d-4266-5fe2-e722-6c8d5bc9919c/00042282762429.rgb.jpg/600x600bb.jpg',
-    appleMusicUrl: 'https://music.apple.com/cn/album/amour-%E9%81%99%E9%81%A0%E7%9A%84%E5%A5%B9/1443314619'
-  },
-  {
-    name: 'rice & shine',
-    artist: '陈奕迅',
-    releasedAt: '2014',
-    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music128/v4/b3/03/0f/b3030fa1-1000-4def-27d3-987b0ed2c69a/00602488972895.rgb.jpg/600x600bb.jpg',
-    appleMusicUrl: 'https://music.apple.com/cn/album/rice-shine/1422694136'
-  },
-  {
-    name: '认了吧',
-    artist: '陈奕迅',
-    releasedAt: '2007',
-    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/27/cb/d6/27cbd6b4-c991-cd95-49e3-b6019462ff97/00602517338845.rgb.jpg/600x600bb.jpg',
-    appleMusicUrl: 'https://music.apple.com/cn/album/%E8%AA%8D%E4%BA%86%E5%90%A7-%E5%8F%B0%E7%81%A3%E7%89%88/1443352354'
-  },
-  {
     name: '跳舞的梵谷',
     artist: '孙燕姿',
     releasedAt: '2017-11-09',
@@ -148,32 +113,18 @@ export const favouriteAlbums: FavouriteAlbum[] = [
     appleMusicUrl: 'https://music.apple.com/cn/album/happier-than-ever/1564530526'
   },
   {
-    name: 'When We All Fall...',
+    name: 'WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?',
     artist: 'Billie Eilish',
     releasedAt: '2019-03-29',
     cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/1a/37/d1/1a37d1b1-8508-54f2-f541-bf4e437dda76/19UMGIM05028.rgb.jpg/600x600bb.jpg',
     appleMusicUrl: 'https://music.apple.com/cn/album/when-we-all-fall-asleep-where-do-we-go/1450695723'
   },
   {
-    name: "Don't Smile at Me",
+    name: 'dont smile at me',
     artist: 'Billie Eilish',
     releasedAt: '2017-08-17',
     cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/02/1d/30/021d3036-5503-3ed3-df00-882f2833a6ae/17UM1IM17026.rgb.jpg/600x600bb.jpg',
     appleMusicUrl: 'https://music.apple.com/cn/album/dont-smile-at-me/1440898929'
-  },
-  {
-    name: 'thank u, next',
-    artist: 'Ariana Grande',
-    releasedAt: '2019-02-08',
-    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/bb/69/07/bb6907de-8ad4-970b-3311-121320e1bf9c/19UMGIM03691.rgb.jpg/600x600bb.jpg',
-    appleMusicUrl: 'https://music.apple.com/cn/album/thank-u-next/1450333975'
-  },
-  {
-    name: 'Sweetener',
-    artist: 'Ariana Grande',
-    releasedAt: '2018-08-17',
-    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/70/53/76/705376dc-d0dc-6945-7fb2-415038e3aafa/18UMGIM36925.rgb.jpg/600x600bb.jpg',
-    appleMusicUrl: 'https://music.apple.com/cn/album/sweetener/1399202900'
   },
   {
     name: 'Chopin: Nocturnes',
@@ -181,5 +132,243 @@ export const favouriteAlbums: FavouriteAlbum[] = [
     releasedAt: '1967',
     cover: 'https://is1-ssl.mzstatic.com/image/thumb/Features125/v4/fa/f7/54/faf7540e-3346-b548-55d8-8617ce707554/dj.hbpmuqwc.jpg/600x600bb.jpg',
     appleMusicUrl: 'https://music.apple.com/cn/album/chopin-nocturnes/594180316'
+  },
+  {
+    name: 'A Matter of Time (Standard Edition)',
+    artist: 'Laufey',
+    releasedAt: '2025-08-24',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/59/46/5f/59465f0d-a8e7-359d-05b9-788541120359/199538409437.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/a-matter-of-time-standard-edition/1834261308'
+  },
+  {
+    name: '空中飞人',
+    artist: '窦靖童',
+    releasedAt: '2024-09-09',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/a9/cd/9b/a9cd9b88-f4e3-674c-91fc-275fcfd7adec/704537426004.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E7%A9%BA%E4%B8%AD%E9%A3%9B%E4%BA%BA/1766912573'
+  },
+  {
+    name: 'My 21st Century Blues',
+    artist: 'RAYE',
+    releasedAt: '2023-02-03',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/90/93/08/909308e3-94ca-dbf8-ebc7-0177bae4b9ad/197186999980.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/us/album/my-21st-century-blues/1647623993'
+  },
+  {
+    name: 'HIT ME HARD AND SOFT',
+    artist: 'Billie Eilish',
+    releasedAt: '2024-05-17',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/92/9f/69/929f69f1-9977-3a44-d674-11f70c852d1b/24UMGIM36186.rgb.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/hit-me-hard-and-soft/1739659134'
+  },
+  {
+    name: 'Javelin',
+    artist: 'Sufjan Stevens',
+    releasedAt: '2023-10-06',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/80/e9/61/80e96151-6e40-6daf-bdc6-33dbb65510e6/40697.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/us/album/javelin/1700723922'
+  },
+  {
+    name: 'Stick Season (Forever)',
+    artist: 'Noah Kahan',
+    releasedAt: '2024-02-09',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/86/93/ce/8693cea7-0f44-ea2a-12bd-d5b4c9c78851/24UMGIM07395.rgb.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/stick-season-forever/1728500403'
+  },
+  {
+    name: '冀西南林路行',
+    artist: '万能青年旅店',
+    releasedAt: '2020-12-22',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/cf/df/2a/cfdf2a8a-0298-ef1d-02a2-f605586b75d4/cover.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/hk/album/%E5%86%80%E8%A5%BF%E5%8D%97%E6%9E%97%E8%B7%AF%E8%A1%8C/1545534900'
+  },
+  {
+    name: '沙发海',
+    artist: '陈绮贞',
+    releasedAt: '2018-12-24',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/13/91/4c/13914cd2-e84d-d890-1e60-4c723b3d2e27/5616804.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E6%B2%99%E7%99%BC%E6%B5%B7/1598248904'
+  },
+  {
+    name: '近人可读',
+    artist: '寸铁',
+    releasedAt: '2020-06-19',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/7d/78/22/7d7822a2-ceae-b3c0-284d-d4fac22079e2/cccccc.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E8%BF%91%E4%BA%BA%E5%8F%AF%E8%AE%80/1519218381'
+  },
+  {
+    name: 'Norman F*****g Rockwell!',
+    artist: 'Lana Del Rey',
+    releasedAt: '2019-08-30',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/c6/5f/b9/c65fb9eb-da2f-89a9-b640-2fff1fc3a660/19UMGIM61350.rgb.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/us/album/norman-f-g-rockwell/1474669063'
+  },
+  {
+    name: 'The City Holds My Heart - EP',
+    artist: 'Ghostly Kisses',
+    releasedAt: '2018-11-23',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/62/8a/ab/628aabda-6f24-7add-73af-0e5330337811/193483836713.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/the-city-holds-my-heart-ep/1463418781'
+  },
+  {
+    name: '飞行器的执行周期',
+    artist: '郭顶',
+    releasedAt: '2016-11-25',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/4b/55/95/4b5595a2-5d2c-38c7-8179-0a27e5ba58a1/00602488600859.rgb.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E9%A3%9B%E8%A1%8C%E5%99%A8%E7%9A%84%E5%9F%B7%E8%A1%8C%E9%80%B1%E6%9C%9F/1443637757'
+  },
+  {
+    name: '安和桥北',
+    artist: '宋冬野',
+    releasedAt: '2013-08-30',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/f7/a9/d7/f7a9d7be-7e4f-eafa-f5c1-bb769249e00b/25UMGIM42190.rgb.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E5%AE%89%E5%92%8C%E6%A1%A5%E5%8C%97/1808446482'
+  },
+  {
+    name: '21',
+    artist: 'Adele',
+    releasedAt: '2011-01-21',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/f8/df/0a/f8df0ac9-ae76-9dae-86d3-4e913fc54fb1/634904152062.png/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/21/403037872'
+  },
+  {
+    name: 'Carrie & Lowell (10th Anniversary Edition)',
+    artist: 'Sufjan Stevens',
+    releasedAt: '2015-03-31',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/bf/77/20/bf772094-afd5-407a-4316-4edf99c10707/52178.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/us/album/carrie-lowell-10th-anniversary-edition/1794841863'
+  },
+  {
+    name: '北方北方',
+    artist: '文静',
+    releasedAt: '2019-01-09',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/0e/08/db/0e08db66-cca6-8282-6618-b07da1e23b77/4894944788638.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E5%8C%97%E6%96%B9%E5%8C%97%E6%96%B9-ep/1775663495'
+  },
+  {
+    name: '独立日',
+    artist: '金海心',
+    releasedAt: '2006-01-11',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music/1f/bb/ac/mzi.wusyzpfn.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E7%8B%AC%E7%AB%8B%E6%97%A5/369654520'
+  },
+  {
+    name: '万能青年旅店',
+    artist: '万能青年旅店',
+    releasedAt: '2010-11-12',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/62/43/86/6243868c-c362-74d0-68d5-1de5e89986ee/cover.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E4%B8%87%E8%83%BD%E9%9D%92%E5%B9%B4%E6%97%85%E5%BA%97/1538548871'
+  },
+  {
+    name: '黑色柳丁',
+    artist: '陶喆',
+    releasedAt: '2002-08-09',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/05/7a/97/057a9723-2dbd-873b-81e0-5090879b2942/825646245352.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E9%BB%91%E8%89%B2%E6%9F%B3%E4%B8%81/914664926'
+  },
+  {
+    name: '太阳',
+    artist: '陈绮贞',
+    releasedAt: '2009-01-22',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/e7/44/26/e744264b-f539-ede8-8527-1ee2f1d13f38/37638.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E5%A4%AA%E9%99%BD/1598246570'
+  },
+  {
+    name: 'Sing and Dance',
+    artist: 'Sophie Zelmani',
+    releasedAt: '2001-12-27',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/47/bd/14/47bd14ab-4098-de28-bfb9-840794f75873/mzi.kwtmdhcy.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/sing-and-dance/213134277'
+  },
+  {
+    name: '我很忙',
+    artist: '周杰伦',
+    releasedAt: '2007-11-02',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/b8/83/7f/b8837f2a-324a-f6ad-ff5a-0805e9210181/JAY.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E6%88%91%E5%BE%88%E5%BF%99/536030690'
+  },
+  {
+    name: 'Di-Dar',
+    artist: '王菲',
+    releasedAt: '1995-12-01',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music118/v4/b5/03/3c/b5033c4e-b34a-7616-623a-3b8c62eab8dc/00044003842427.rgb.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/di-dar/1443352171'
+  },
+  {
+    name: '陶喆同名专辑',
+    artist: '陶喆',
+    releasedAt: '1997-12-06',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/dd/bb/bd/ddbbbda1-9408-72b2-4c0b-943e648be6c1/190295574086.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E9%99%B6%E5%96%86%E5%90%8C%E5%90%8D%E5%B0%88%E8%BC%AF/1416149926'
+  },
+  {
+    name: '黑豹',
+    artist: '黑豹乐队',
+    releasedAt: '1992-12-15',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Features115/v4/d3/0a/d3/d30ad392-142d-2d60-f721-16028ae68ebb/dj.lmoukelr.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E9%BB%91%E8%B1%B9/177025348'
+  },
+  {
+    name: '黑梦',
+    artist: '窦唯',
+    releasedAt: '1994-10-06',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Features115/v4/f9/ac/98/f9ac98e4-956f-c013-e703-8ae5b7cdb880/dj.flkwjbql.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E9%BB%91%E5%A4%A2/158796449'
+  },
+  {
+    name: 'The Dark Side of the Moon (50th Anniversary) [Remastered]',
+    artist: 'Pink Floyd',
+    releasedAt: '1973-03-01',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/49/86/18/49861852-877b-0992-fa27-58b25fa032b5/196589805232.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/us/album/the-dark-side-of-the-moon-50th-anniversary-remastered/1665303755'
+  },
+  {
+    name: '天空',
+    artist: '王菲',
+    releasedAt: '1994-11-10',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music118/v4/67/67/2f/67672fd2-2592-3cca-2829-1ce28c775992/00044003839229.rgb.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E5%A4%A9%E7%A9%BA/1443363496'
+  },
+  {
+    name: 'No Need To Argue',
+    artist: 'The Cranberries',
+    releasedAt: '1994-10-03',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/a3/77/a3/a377a309-8e52-f787-2250-2b34d320bf7b/25UMGIM64146.rgb.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/no-need-to-argue-2025-remastered/1828830621'
+  },
+  {
+    name: '他们说忘了摇滚有问题',
+    artist: '腰乐队',
+    releasedAt: '2008-01-30',
+    cover: 'https://cdn-images.dzcdn.net/images/cover/d4b51c1b590757bc4e78502f909ec228/1000x1000-000000-80-0-0.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/search?term=%E4%BB%96%E4%BB%AC%E8%AF%B4%E5%BF%98%E4%BA%86%E6%91%87%E6%BB%9A%E6%9C%89%E9%97%AE%E9%A2%98'
+  },
+  {
+    name: '相见恨晚',
+    artist: '腰乐队',
+    releasedAt: '2014-07-24',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/04/c5/f1/04c5f14d-37f4-1c8c-a4d7-bc59f281672e/mzm.bswxcero.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E7%9B%B8%E8%A7%81%E6%81%A8%E6%99%9A/1053358611'
+  },
+  {
+    name: '这个世界会好吗',
+    artist: '李志',
+    releasedAt: '2006-11-20',
+    cover: 'https://cdn-images.dzcdn.net/images/cover/8487c8629712fa45bdc4c514b2d24f3f/1000x1000-000000-80-0-0.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/search?term=%E8%BF%99%E4%B8%AA%E4%B8%96%E7%95%8C%E4%BC%9A%E5%A5%BD%E5%90%97%20%E6%9D%8E%E5%BF%97'
+  },
+  {
+    name: '丑奴儿',
+    artist: '草东没有派对',
+    releasedAt: '2016-02-19',
+    cover: 'https://coverartarchive.org/release-group/0436f306-0993-4e12-acfa-4b725163b9bd/front-500',
+    appleMusicUrl: 'https://music.apple.com/cn/search?term=%E4%B8%91%E5%A5%B4%E5%84%BF%20%E8%8D%89%E4%B8%9C%E6%B2%A1%E6%9C%89%E6%B4%BE%E5%AF%B9'
+  },
+  {
+    name: '再想想',
+    artist: '宋冬野',
+    releasedAt: '2026-06-29',
+    cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/45/c9/fe/45c9febc-d55c-62ef-c168-9cb013b9db65/26UMGIM69223.rgb.jpg/600x600bb.jpg',
+    appleMusicUrl: 'https://music.apple.com/cn/album/%E5%86%8D%E6%83%B3%E6%83%B3/6777959168'
   }
 ]
